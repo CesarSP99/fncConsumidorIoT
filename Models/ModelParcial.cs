08d4685f7eaa24faf9561c6496b96623f5d2df19
CesarSP99 <cesarandres00@gmail.com>
2021-04-20 20:12:54 -0400

    Modelo agregado

---
+++ Models/ModelParcial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace fncConsumidorIoT.Models
+{
+    class ModelParcial
+    {
+        [Key]
+        public int messageId { get; set; }
+        [Required]
+        public string estudiante { get; set; }
+        [Required]
+        public double temperatura { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime fecha { get; set; }
+        [Required]
+        public string hora { get; set; }
+    }
+}
