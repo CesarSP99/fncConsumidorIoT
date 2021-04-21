608aa263fb8e7bce9a58217dbd82947aa06a6acf
CesarSP99 <cesarandres00@gmail.com>
2021-04-20 20:23:12 -0400

    Codigo para deserializar y mandar a la base de datos agregado.

--- Function1.cs
+++ Function1.cs
@@ -1,16 +1,31 @@
 using System;
+using fncConsumidorIoT.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace fncConsumidorIoT
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([ServiceBusTrigger("colaparcial2", Connection = "MyQueueConn")]string myQueueItem, ILogger log)
+        public static async System.Threading.Tasks.Task RunAsync(
+            [ServiceBusTrigger("colaparcial2", Connection = "MyQueueConn")]string myQueueItem,
+            [CosmosDB(databaseName: "dbParcial2", collectionName: "ModelParcial", ConnectionStringSetting = "CosmosConn")] IAsyncCollector<object> datos,
+            ILogger log
+            )
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            try
+            {
+                log.LogInformation($"Se proceso el siguiente mensaje: {myQueueItem}");
+                var data = JsonConvert.DeserializeObject<ModelParcial>(myQueueItem);
+                await datos.AddAsync(data);
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"No se puede agregar datos: {ex.Message}");
+            }
         }
     }
 }
